a861fc7a21e4b3bc07890caf0627971e6369221a
KajalAHP <kajal.adhikari.hp@gmail.com>
2021-10-21 18:50:39 +0530

    add and delete functionality

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,50 @@
-import React,{useState}from "react";
- 
-function Todos(){
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
+import { addTodos } from "../redux/reducer";
 
- return(
-     <div>
-         <div>
-             <label>
-                 <input type="text" />
-                 <button>Add</button>
-             </label>
-         </div>
-     </div>
- )
- }
- export default Todos;
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const add = () => {
+    if (todo === "") {
+      alert("empty");
+    } else {
+      props.addTodo({
+        id:  Date.now(),
+        item: todo,
+      });
+      setTodo("");
+      console.log(props.addTodo);
+    }
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <label>
+          <input type="text" onChange={(e) => handleChange(e)} value={todo} />
+          <button onClick={() => add()}>Add</button>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
