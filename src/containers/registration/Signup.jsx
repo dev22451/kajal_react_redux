6fbb81d6a5c35f61a4a1258749f362241fb4356e
KajalAHP <kajal.adhikari.hp@gmail.com>
2021-10-29 19:31:40 +0530

    Added form validation and ESLint congiguration

--- src/containers/registration/Signup.jsx
+++ src/containers/registration/Signup.jsx
@@ -1,70 +1,172 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from "react";
-import './style.css'
-const Signup = () =>{
-    const[userRegistration, setUserRegistration] = useState({
-       FirstName:"",
-       LastName:"",
-       Username:"",
-       Email:"",
-       Password:"",
-       ConfirmPassword:"",
-    });
+import "./style.css";
 
+const Signup = () => {
+  const [userRegistration, setUserRegistration] = useState({
+    FirstName: "",
+    LastName: "",
+    Username: "",
+    Email: "",
+    Password: "",
+    ConfirmPassword: "",
+  });
+  const [checkPassword, setCheckPassword] = useState(false);
 
-    const handleInput = (e) =>{
-      
-
-
+  const handleSubmit = () => {
+    if (userRegistration.Password === userRegistration.ConfirmPassword) {
+      setCheckPassword(true);
+    } else {
+      setCheckPassword(false);
     }
-    return(
-        <>
-        <form className="for">
-           <h1>Sign Up</h1>
-           
-            <div className="field">
-                <label htmlFor="firstname">First Name</label><br/>
-                <input type="text" autoComplete="off" name="firstname" id="firstname"
-                 value={userRegistration.FirstName} onChange={handleInput}/>
-            </div>
-          
-            <div className="field">
-               <label htmlFor="lastname">Last Name</label><br/>
-               <input type="text" autoComplete="off" name="lastname" id="lastname" 
-               value={userRegistration.LastName} onChange={handleInput}/>
-            </div>
-            
-            <div className="field">
-              <label htmlFor="username">Username</label><br/>
-               <input type="text"  autoComplete="off"  name="username" id="username"
-                value={userRegistration.Username} onChange={handleInput}/>
-            </div>
+  };
 
-            <div className="field">
-               <label htmlFor="email">Email</label><br/>
-               <input type="text" autoComplete="off" name="email" id="email" 
-               value={userRegistration.Email} onChange={handleInput}/>
-            </div>
+  const handleInput = (e) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    console.log(name, value);
+    setUserRegistration({ ...userRegistration, [name]: value });
+  };
+  return (
+    <>
+      <form className="for">
+        <h1>Sign Up</h1>
 
-            <div className="field password">
-               <label htmlFor="password">Password</label><br/>
-               <input type="text" autoComplete="off" name="password" id="password" 
-               value={userRegistration.Password} onChange={handleInput}/>
-            </div>
+        <div className="field">
+          <label htmlFor="FirstName">First Name</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/747/747376.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              name="FirstName"
+              id="firstname"
+              value={userRegistration.FirstName}
+              onChange={handleInput}
+            />
+          </div>
+        </div>
 
-            <div className="field">
-               <label htmlFor="confirmpassword">Confirm Password</label><br/>
-               <input type="text" autoComplete="off" name="password" id="password" 
-               value={userRegistration.ConfirmPassword}onChange={handleInput}/><br/><br/>
-            </div>
+        <div className="field">
+          <label htmlFor="LastName">Last Name</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/747/747376.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              name="LastName"
+              id="lastname"
+              value={userRegistration.LastName}
+              onChange={handleInput}
+            />
+          </div>
+        </div>
 
-            <button type="button">Register</button>
-           
+        <div className="field">
+          <label htmlFor="Username">Username</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/747/747376.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              name="Username"
+              id="username"
+              value={userRegistration.Username}
+              onChange={handleInput}
+            />
+          </div>
+        </div>
 
+        <div className="field">
+          <label htmlFor="Email">Email</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/561/561127.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="text"
+              autoComplete="off"
+              name="Email"
+              id="email"
+              value={userRegistration.Email}
+              onChange={handleInput}
+            />
+          </div>
+        </div>
 
+        <div className="field">
+          <label htmlFor="Password">Password</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3064/3064197.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="password"
+              autoComplete="off"
+              name="Password"
+              id="password"
+              value={userRegistration.Password}
+              onChange={handleInput}
+            />
+          </div>
+        </div>
 
-        </form>
-        </>
+        <div className="field">
+          <label htmlFor="ConfirmPassword">Confirm Password</label>
+          <br />
+          <div className="action">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/3064/3064197.png"
+              alt=""
+              width="20px"
+              height="auto"
+            />
+            <input
+              type="password"
+              autoComplete="off"
+              name="ConfirmPassword"
+              id="confirmPassword"
+              value={userRegistration.ConfirmPassword}
+              onChange={handleInput}
+            />
+            <br />
+          </div>
+        </div>
 
-    )
-}
-export default Signup
+        <button type="button" className="btn" onClick={handleSubmit}>
+          Register
+        </button>
+        {checkPassword === false && userRegistration.ConfirmPassword !== "" ? (
+          <span>Passwords are not equal</span>
+        ) : null}
+      </form>
+    </>
+  );
+};
+export default Signup;
